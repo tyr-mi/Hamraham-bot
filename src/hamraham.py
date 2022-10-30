66e607409b344d412ffeec86dc4ca187d9361b77
a.mianji <a.mianji@sahra.ir>
2022-10-30 17:21:50 +0330

    Added poem files

--- src/hamraham.py
+++ src/hamraham.py
@@ -4,14 +4,27 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
+import sqlite3
+import os
+import random
 
 updater = Updater("5583347821:AAHrlqHXfyIMJ880PRqFUvX-b8WXh6jt6-k",
                   use_context=True)
   
-  
+
+def get_db_connection():
+    connection = sqlite3.connect("src" + os.sep + "Hafez.db")
+    return connection
+    
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Enter the text you want to show to the user whenever they start the bot")
+    connection = get_db_connection()
+    selected_poem = random.randint(0,415)
+    query = "SELECT TEXT FROM poems WHERE id = " + str(selected_poem)
+    cur = connection.cursor()
+    cur.execute(query)
+    poem = cur.fetchall()
+    print(poem)
+    update.message.reply_text(poem)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 
---
+++ src/test.py
@@ -0,0 +1,20 @@
+import re
+import sqlite3
+import os
+import random
+
+
+def get_db_connection():
+    connection = sqlite3.connect("src" + os.sep + "Hafez.db")
+    return connection
+  
+def start():
+    connection = get_db_connection()
+    selected_poem = random.randint(0,415)
+    query = "SELECT TEXT FROM poems WHERE id = " + str(selected_poem)
+    cur = connection.cursor()
+    cur.execute(query)
+    poem = cur.fetchall()
+    print(poem)
+
+start()
