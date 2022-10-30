33640c3543554a26c5b7c3941cf50cd7f3d5e6f4
a.mianji <a.mianji@sahra.ir>
2022-10-30 17:26:34 +0330

    Bug fixed

--- src/hamraham.py
+++ src/hamraham.py
@@ -15,14 +15,14 @@ updater = Updater("5583347821:AAHrlqHXfyIMJ880PRqFUvX-b8WXh6jt6-k",
 def get_db_connection():
     connection = sqlite3.connect("src" + os.sep + "Hafez.db")
     return connection
-    
+
 def start(update: Update, context: CallbackContext):
     connection = get_db_connection()
     selected_poem = random.randint(0,415)
     query = "SELECT TEXT FROM poems WHERE id = " + str(selected_poem)
     cur = connection.cursor()
     cur.execute(query)
-    poem = cur.fetchall()
+    poem = cur.fetchone()
     print(poem)
     update.message.reply_text(poem)
 
