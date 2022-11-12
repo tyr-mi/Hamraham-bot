0293bb5d8582d1a2277cbc499d287217749b1308
a.mianji <a.mianji@sahra.ir>
2022-11-12 11:17:43 +0330

    bug fix

--- src/helpers/poem_repository.py
+++ src/helpers/poem_repository.py
@@ -5,13 +5,12 @@ class PoemRepository:
     poems = []
 
     def __init__(self) -> None:
-        poems = helpers.database_helper.get_all_poems()
+        self.poems = helpers.database_helper.get_all_poems()
 
     def shuffle(self):
-        shuffeled_array = random.shuffle(self.poems)
-        return shuffeled_array
+        random.shuffle(self.poems)
 
     def retrieve_poem(self):
-        shuffeled_arr = self.shuffle()
+        self.shuffle()
         selected_poem = random.randint(1,495)
-        return shuffeled_arr[selected_poem]
+        return self.poems[selected_poem]
---
+++ src/poem_test.py
@@ -0,0 +1,7 @@
+import sys, os
+
+from helpers.poem_repository import PoemRepository 
+
+repo = PoemRepository()
+
+poem = repo.retrieve_poem()
